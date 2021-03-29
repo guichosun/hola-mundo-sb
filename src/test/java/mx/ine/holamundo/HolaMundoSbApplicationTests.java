3eda8ed11581c52a10e72308b0b92836e0787b4a
alberto.delcampo-at-676770050448 <luis.albertodelcampo@gmail.com>
2021-03-29 17:10:43 -0600

    El inicio

---
+++ src/test/java/mx/ine/holamundo/HolaMundoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ine.holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
